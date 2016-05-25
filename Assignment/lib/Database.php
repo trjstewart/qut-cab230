1639c588680a00bf0dc7bbad5d760b42df4841c0
Tylor Stewart <trjstewart@gmail.com>
2016-05-25 14:48:27 +1000

    Database Class Constructor and Singleton

--- Assignment/lib/Database.php
+++ Assignment/lib/Database.php
@@ -1,17 +1,32 @@
 <?php
 
 class Database {
-    const HOST = 'srv01.binaryorange.co';
-    const NAME = 'cab230_cab230';
-    const USER = 'cab230_cab230';
-    const PASS = 'tRKtidLWSOBwh4G';
+    // Database Configuration Variables
+    private static $HOST = 'srv01.binaryorange.co';     // Database URL
+    private static $NAME = 'cab230_cab230';             // Database Name
+    private static $USER = 'cab230_cab230';             // Database Username
+    private static $PASS = 'tRKtidLWSOBwh4G';           // Database Password
 
-    function __construct() {
+    public $dbh;                // Database Handler
+    private static $instance;   // Database Instance
+
+    // Constructor: Try to connect to the database and if not throw an error.
+    private function __construct() {
         try {
-            $dbh = new PDO('mysql:host='.self::HOST.';dbname='.self::NAME, self::USER, self::PASS);
+            $this->dbh = new PDO('mysql:host='.self::$HOST.';dbname='.self::$NAME, self::$USER, self::$PASS);
         } catch (PDOException $e) {
             print 'Error: ' . $e->getMessage() . '<br>';
             die();
         }
+    }
+
+    // Returns Singleton Class.
+    public static function getInstance() {
+        if (!isset(self::$instance)) {
+            $obj = __CLASS__;
+            self::$instance = new $obj;
+        }
+
+        return self::$instance;
     }
 }
