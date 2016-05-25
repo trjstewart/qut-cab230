b19a41f4722e4896284f0bf959f925803e3e9dc3
Tylor Stewart <trjstewart@gmail.com>
2016-05-25 12:42:50 +1000

    Database Class

---
+++ Assignment/lib/Database.php
@@ -0,0 +1,16 @@
+<?php
+
+class Database {
+    const HOST = 'srv01.binaryorange.co';
+    const NAME = 'cab230';
+    const USER = 'cab230_cab230';
+    const PASS = 'tRKtidLWSOBwh4G';
+
+    function __construct() {
+        try {
+            $dbh = new PDO('mysql:host='.self::HOST.';dbname='.self::NAME, self::USER, self::PASS);
+        } catch (PDOException $e) {
+            print 'Error: ' . $e->getMessage() . '<br>';
+        }
+    }
+}
